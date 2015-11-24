28c997859bf5d67b682db327b4d1f7d998646127
Matt Gaspar <matt.gaspar@digital.landregistry.gov.uk>
2015-11-24 16:08:23 +0000

    Initial Unit Test

--- tests.sh
+++ tests.sh
@@ -1,6 +1,4 @@
 env_dir="$JENKINS_HOME/virtualenv/${JOB_NAME// /_}"
 virtualenv $env_dir
 . $env_dir/bin/activate
-echo `which pip`
-echo `pip --version`
-#install -r requirements.txt
+pip install -r requirements.txt
