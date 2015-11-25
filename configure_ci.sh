c4904f378637526692fd5068e017d180c93fb417
Matt Gaspar <matt.gaspar@digital.landregistry.gov.uk>
2015-11-25 09:07:43 +0000

    New configuration

--- configure_ci.sh
+++ configure_ci.sh
@@ -1,6 +1,5 @@
-# env_dir="$JENKINS_HOME/virtualenv/${JOB_NAME// /_}"
-env_dir="$JENKINS_HOME/virtualenv/${JOB_NAME}"
+export env_dir="$JENKINS_HOME/virtualenv/${JOB_NAME// /_}"
 echo $env_dir
-#virtualenv $env_dir
-#. $env_dir/bin/activate
-#pip install -r requirements.txt
+virtualenv $env_dir
+. $env_dir/bin/activate
+pip install -r requirements.txt
