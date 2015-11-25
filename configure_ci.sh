48a846cef35c1022e27a56ce9c13e741ad3b5c6a
Matt Gaspar <matt.gaspar@digital.landregistry.gov.uk>
2015-11-25 08:48:26 +0000

    New configuration

--- configure_ci.sh
+++ configure_ci.sh
@@ -1,4 +1,5 @@
-env_dir="$JENKINS_HOME/virtualenv/${JOB_NAME// /_}"
+# env_dir="$JENKINS_HOME/virtualenv/${JOB_NAME// /_}"
+env_dir="$JENKINS_HOME/virtualenv/${JOB_NAME}"
 echo $env_dir
 #virtualenv $env_dir
 #. $env_dir/bin/activate
