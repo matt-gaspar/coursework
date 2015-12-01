b4edccfe5ea34f7d71fe07d0473cb8af31d30602
Matt Gaspar <matt.gaspar@digital.landregistry.gov.uk>
2015-12-01 15:31:41 +0000

    Updated test script

--- run_tests.sh
+++ run_tests.sh
@@ -2,4 +2,4 @@
 
 # Run unit tests
 mkdir -p test-reports
-py.test --ignore=venv --junitxml=test-reports/test-hello.xml --cov-report term-missing --cov-report=html --cov tests/
+py.test --ignore=venv --junitxml=test-reports/test-hello.xml --cov-report term-missing --cov-report=html --cov . tests
